0f5c916158044bdb675d5c6f184de9ab3b552629
Dan Kreiger <dan@dankreiger.com>
2018-11-05 22:55:01 +0100

    test: update home spec

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -3,22 +3,31 @@ import { shallow } from 'enzyme';
 
 import Home from './Home';
 import Root from '../../Root';
+import SimpleMap from '../SimpleMap/SimpleMap';
+import { setupGoogleMock } from '../../utils/mocks';
 
 describe('Home', () => {
   let homeComponent;
 
+  beforeAll(() => {
+    setupGoogleMock();
+  });
+
   beforeEach(() => {
-    homeComponent = shallow(
-      <Root>
-        <Home />
-      </Root>
-    );
+    homeComponent = shallow(<Home />);
   });
   describe('rendering', () => {
     test('renders as expected', () => {
       expect(homeComponent).toBeTruthy();
       expect(homeComponent.length).toBe(1);
       expect(homeComponent).toMatchSnapshot();
+    });
+  });
+
+  describe('structure', () => {
+    test('renders 1 map component', () => {
+      expect(homeComponent.find(SimpleMap)).toBeTruthy();
+      expect(homeComponent.find(SimpleMap).length).toBe(1);
     });
   });
 });
